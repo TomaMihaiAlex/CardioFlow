57abd8bbbc6ca79207a0124b8c5d83f055e60148
A13xTheITGuy <alexstandaniel@gmail.com>
2026-05-21 13:01:04 +0300

    Commit

--- app/src/main/java/com/example/cardioflow/activities/MainActivity.java
+++ app/src/main/java/com/example/cardioflow/activities/MainActivity.java
@@ -1,11 +1,19 @@
-package com.example.cardioflow;
+package com.example.cardioflow.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import com.example.cardioflow.auth.AuthManager;
+
+import com.example.cardioflow.HistoryFragment;
+import com.example.cardioflow.HomeFragment;
+import com.example.cardioflow.R;
+import com.example.cardioflow.RecommendationsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (!AuthManager.getInstance(this).isLoggedIn()) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -34,5 +49,22 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, fragment);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            AuthManager.getInstance(this).logout();
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
