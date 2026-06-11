742b82b60e9ef750391dfb6866524f6f0f6380e3
A13xTheITGuy <alexstandaniel@gmail.com>
2026-06-11 04:30:24 +0300

    Setare targetSdk = 35 pentru compatibilitate și stabilitate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.cardioflow"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
